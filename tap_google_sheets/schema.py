c80a0b4015ce69b563af284ef16a783e7d258870
scott.coleman <scott.coleman@bytecode.io>
2020-03-27 17:10:31 -0500

    Add pad to columns without data and ignore hidden sheets

--- tap_google_sheets/schema.py
+++ tap_google_sheets/schema.py
@@ -19,21 +19,28 @@ def colnum_string(num):
     return string
 
 
+def pad_default_effective_values(headers, first_values):
+    for i in range(len(headers) - len(first_values)):
+        first_values.append(OrderedDict())
+
+
 # Create sheet_metadata_json with columns from sheet
 def get_sheet_schema_columns(sheet):
     sheet_title = sheet.get('properties', {}).get('title')
     sheet_json_schema = OrderedDict()
     data = next(iter(sheet.get('data', [])), {})
     row_data = data.get('rowData', [])
-    if row_data == []:
-        # Empty sheet, SKIP
+    if row_data == [] or len(row_data) == 1:
+        # Empty sheet or empty first row, SKIP
         LOGGER.info('SKIPPING Empty Sheet: {}'.format(sheet_title))
         return None, None
     else:
         # spreadsheet is an OrderedDict, with orderd sheets and rows in the repsonse
         headers = row_data[0].get('values', [])
         first_values = row_data[1].get('values', [])
-        # LOGGER.info('first_values = {}'.format(json.dumps(first_values, indent=2, sort_keys=True)))
+        # Pad first row values with default if null
+        if len(first_values) < len(headers):
+            pad_default_effective_values(headers, first_values)
 
         sheet_json_schema = {
             'type': 'object',
