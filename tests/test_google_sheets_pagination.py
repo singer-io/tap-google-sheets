93185b6860e828f00b7117a1979d32ee5d4a2375
prijendev <prijen.khokhani@crestdatasys.com>
2021-12-02 17:13:58 +0530

    Added comments

--- tests/test_google_sheets_pagination.py
+++ tests/test_google_sheets_pagination.py
@@ -73,18 +73,27 @@ class PaginationTest(GoogleSheetsBaseTest):
                     # Verify by primary keys that data is unique for page
                     self.assertTrue(
                         primary_keys_page_1.isdisjoint(primary_keys_page_2))
-
+                
                 if stream == "sadsheet-pagination":
                     # verify the data for the "sadsheet-pagination" stream is free of any duplicates or breaks by checking
-                    # our fake pk value ('id')
-                    expected_pk_list = list(range(1, 238))
+                    # our fake pk value ('id'). 'id' is column in sheet which contains unique index 1 to 237
+                    expected_pk_list = list(range(1, 238)) # Return 1 to 237
+
+                    # Remove 198 and 199 from expected_pk_list because id by this number does not exist. 
+                    # That means those two rows are blank.
                     expected_pk_list = [x for x in expected_pk_list if x not in [198, 199]]
+
+                    # This assertion can be made because the data is in a specific way.
                     self.assertEqual(expected_pk_list, fake_pk_list)
                     
                     # verify the data for the "sadsheet-pagination" stream is free of any duplicates or breaks by checking
                     # the actual primary key values (__sdc_row)
-                    expected_pk_list = list(range(2, 239))
+                    expected_pk_list = list(range(2, 239)) # Return 2 to 238. Because 1st row contains header values itself.
+
+                    # Remove 199 and 200 from expected_pk_list because __sdc_row by this number does not exist. 
+                    # That means those two rows are blank.
                     expected_pk_list = [x for x in expected_pk_list if x not in [199, 200]]
+
                     self.assertEqual(expected_pk_list, actual_pk_list)
                     
                     continue
