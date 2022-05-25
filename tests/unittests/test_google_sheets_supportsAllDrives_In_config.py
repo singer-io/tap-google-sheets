4f004d90363a96ff145d7f50bdf309725cd43f3e
Jay Tilala <jay.tilala@CDSYS.LOCAL>
2022-05-25 18:34:52 +0530

    update comments in unittest

--- tests/unittests/test_google_sheets_supportsAllDrives_In_config.py
+++ tests/unittests/test_google_sheets_supportsAllDrives_In_config.py
@@ -13,7 +13,7 @@ class Testsupports_all_drives(unittest.TestCase):
     def test_supports_all_drives_other_than_true_in_config(self):
         """To verify that when the supports_all_drives value is given other than true value in config.json then set default value False"""
         
-        # provide supports_all_drives other then boolean value
+        # provide supports_all_drives other then true value
         supports_all_drives = 123
         client =  GoogleClient('test', 'test', 'test', None, 'test', supports_all_drives)
         self.assertEqual(client.supports_all_drives, False, "supports_all_drives got unexpected value")
@@ -21,7 +21,7 @@ class Testsupports_all_drives(unittest.TestCase):
     def test_supports_all_drives_str_true_in_config(self):
         """To verify that when the supports_all_drives value is given true as a string in config.json then set True"""
         
-        # provide supports_all_drives false as string value
+        # provide supports_all_drives true as string as string value
         supports_all_drives = 'true'
         client =  GoogleClient('test', 'test', 'test', None, 'test', supports_all_drives)
         self.assertEqual(client.supports_all_drives, True, "supports_all_drives got unexpected value")
@@ -29,7 +29,7 @@ class Testsupports_all_drives(unittest.TestCase):
     def test_supports_all_drives_boolean_true_in_config(self):
         """To verify that when the supports_all_drives value is given True as boolean in config.json then use supports_all_drives"""
         
-        # provide supports_all_drives boolean value
+        # provide supports_all_drives True as boolean value
         supports_all_drives = True
         client =  GoogleClient('test', 'test', 'test', None, 'test', supports_all_drives)
         self.assertEqual(client.supports_all_drives, supports_all_drives, "supports_all_drives got unexpected value")
