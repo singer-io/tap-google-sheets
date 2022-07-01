2e8cc37dd4903db721ac3f259c3970db82522318
Jay Tilala <jay.tilala@CDSYS.LOCAL>
2022-07-01 15:21:18 +0530

    Add call count assertion and remove unused import

--- tests/unittests/test_catalog_in_sync.py
+++ tests/unittests/test_catalog_in_sync.py
@@ -1,4 +1,3 @@
-from shutil import ExecError
 import unittest
 from unittest import mock
 from tap_google_sheets import main
@@ -11,19 +10,17 @@ class MockedParseArgs:
     catalog = "test"
     
 @mock.patch("tap_google_sheets.discover")
-@mock.patch("tap_google_sheets.singer.utils.parse_args")
-@mock.patch("tap_google_sheets.GoogleClient.__enter__")
+@mock.patch("tap_google_sheets.singer.utils.parse_args", return_value=MockedParseArgs)
+@mock.patch("tap_google_sheets.GoogleClient.__enter__", return_value="test")
 @mock.patch("tap_google_sheets.sync")
 class TestCatalog(unittest.TestCase):
     def test_catalog_is_given_in_sync(self, mocked_sync, mocked_google_client, mocked_parse_args, mocked_discover):
         """
         To verify that if catalog is given in sync mode then run with catalog file
         """
-        MockedParseArgs.catalog = "test"
-        mocked_parse_args.return_value = MockedParseArgs
-        mocked_google_client.return_value = "test"
         main()
         mocked_sync.assert_called_with(client="test", config=MockedParseArgs.config, catalog="test", state={})
+        self.assertEqual(mocked_discover.call_count, 0, "discover function is not called expected times")
         
     def test_catalog_is_not_given_in_sync(self, mocked_sync, mocked_google_client, mocked_parse_args, mocked_discover):
         """
@@ -32,7 +29,6 @@ class TestCatalog(unittest.TestCase):
         # mocking discover function
         MockedParseArgs.catalog = ""
         mocked_discover.return_value = "test"
-        mocked_parse_args.return_value = MockedParseArgs
-        mocked_google_client.return_value = "test"
         main()
         mocked_sync.assert_called_with(client="test", config=MockedParseArgs.config, catalog="test", state={})
+        self.assertEqual(mocked_discover.call_count, 1, "discover function is not called expected times")
