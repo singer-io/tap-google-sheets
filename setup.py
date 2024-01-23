a958361b07541a8d8b79306dc1766755eb67d940
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2024-01-23 16:16:11 -0500

    Update to python 3.11 (#94)
    * test on python 3.11
    
    * changelog update and requests bump
    
    * keep on standard base image [skip ci]
    
    -----------------------------

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-google-sheets',
-      version='2.0.0',
+      version='2.1.0',
       description='Singer.io tap for extracting data from the Google Sheets v4 API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_google_sheets'],
       install_requires=[
-          'backoff==1.8.0',
-          'requests==2.22.0',
-          'singer-python==5.12.2'
+          'backoff==2.2.1',
+          'requests==2.31.0',
+          'singer-python==6.0.0'
       ],
       extras_require={
           'test': [
