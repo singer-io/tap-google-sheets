9544d36997c4bd1f67941b64e25c5754e53000c7
RushiT0122 <rtodkar@stitchdata-talend.com>
2024-03-07 13:10:32 +0000

    update null time unittest

--- tests/unittests/test_null_cell_format.py
+++ tests/unittests/test_null_cell_format.py
@@ -61,6 +61,28 @@ class TestNullCellFormat(unittest.TestCase):
         # verify the returned schema has expected field types and format
         self.assertIn(expected_format,returned_formats)
 
+    def test_null_date_effectiveFormat(self):
+        """
+        Test when no value is given in second row of date, discovery is locking at 'effectiveFormat'.
+        And returns type and format in schema according to that.
+        """
+        sheet = SHEET
+        sheet["data"][0]["rowData"][1]["values"][0]["effectiveFormat"]["numberFormat"]["type"] = "DATE"
+
+        expected_format = {
+                            "type": [
+                                "null",
+                                "string"
+                            ],
+                            "format": "date-time"
+                        }
+
+        sheet_json_schema, columns = schema.get_sheet_schema_columns(sheet)
+        returned_formats = sheet_json_schema["properties"]["Column1"]["anyOf"]
+
+        # verify the returned schema has expected field types and format
+        self.assertIn(expected_format,returned_formats)
+
     def test_null_time_effectiveFormat(self):
         """
         Test when no value is given in second row of time, discovery is locking at 'effectiveFormat'.
