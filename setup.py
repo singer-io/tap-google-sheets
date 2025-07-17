82249b1b2227cde586265f770a49766605875106
Andy Lu <andy@stitchdata.com>
2025-07-17 14:07:57 -0400

    Bump versions (#98)
    * Bump versions
    
    * Bump versions

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-google-sheets',
-      version='3.1.0',
+      version='3.1.1',
       description='Singer.io tap for extracting data from the Google Sheets v4 API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_google_sheets'],
       install_requires=[
           'backoff==2.2.1',
-          'requests==2.31.0',
-          'singer-python==6.0.0'
+          'requests==2.32.4',
+          'singer-python==6.0.1'
       ],
       extras_require={
           'test': [
