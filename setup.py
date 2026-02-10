4bfb2b033dd46ce359bbebd84553ac3d9dfbad97
Daniel Bohan <dbohan@talend.com>
2026-02-10 21:36:38 +0000

    SAC-30246/standardize-state

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-google-sheets',
       install_requires=[
           'backoff==2.2.1',
           'requests==2.32.4',
-          'singer-python==6.0.1'
+          'singer-python==6.7.0'
       ],
       extras_require={
           'test': [
