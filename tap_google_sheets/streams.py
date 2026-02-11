cbd3c4ce342c636b1f9ab784d0905f91bb47cc11
Daniel Bohan <dbohan@talend.com>
2026-02-11 16:56:22 +0000

    Fix new_format_message

--- tap_google_sheets/streams.py
+++ tap_google_sheets/streams.py
@@ -108,9 +108,9 @@ def get_selected_fields(catalog, stream_name):
             pass
     return selected_fields
 
-def new_format_message(message):
+def new_format_message(message, ensure_ascii=False, allow_nan=False):
     """To override the ensure_ascii param, overwitten this function"""
-    return json.dumps(message.asdict(), ensure_ascii=False, allow_nan=False)
+    return json.dumps(message.asdict(), use_decimal=True, ensure_ascii=False, allow_nan=allow_nan)
 
 # To override the ensure_ascii param as while writing record the currency symbols were written as ascii values,
 # overwitten this function of messages file of the singer module
